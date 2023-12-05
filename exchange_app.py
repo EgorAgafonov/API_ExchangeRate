b52490b663b0f873ad5b26d6c77452e5a197ac6f
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-12-05 17:55:07 +0300

    File updated

--- exchange_app.py
+++ exchange_app.py
@@ -1,19 +1,18 @@
 import json
-
 import requests
-import pytest
 from settings import api_key_valid
 
+a = "https://v6.exchangerate-api.com/v6/b4225086daa983f4e12ee736/latest/"
 
 class ExchangeRateAPI:
     def __init__(self):
-        self.base_url = "https://v6.exchangerate-api.com/v6/YOUR-API-KEY/" + api_key_valid
+        self.base_url = "https://v6.exchangerate-api.com/v6/"
 
     def get_exchange_rate(self, currency):
         """Отправка стандартного GET-запроса для предоставления сведений о текущем курсе выбранной валюты по отношению к
         валютам других стран."""
 
-        response = requests.get(self.base_url + "/latest" + currency)
+        response = requests.get(self.base_url + api_key_valid + "latest/" + currency)
 
         status = response.status_code
         result = ""
--- tests/tests_exchangerate_positive.py
+++ tests/tests_exchangerate_positive.py
@@ -10,4 +10,10 @@ class TestExchangeRatePositive:
     def test_get_exchange_rate_USD(self):
         """"""
         status, result = ER.get_exchange_rate("USD")
-        print(f"{status}\n{result}")
+        print(f"\n{status}")
+        list_of_items = dict(result).items()
+        print(result, sep="\n")
+
+        # assert status == "200"
+
+

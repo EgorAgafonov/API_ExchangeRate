b26165abd8fe6bf20317cc10694d1272a5b53274
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-12-05 21:43:27 +0300

    File updated

--- tests/tests_exchangerate_positive.py
+++ tests/tests_exchangerate_positive.py
@@ -5,16 +5,18 @@ ER = ExchangeRateAPI()
 
 
 class TestExchangeRatePositive:
-    """"""
+    """Класс с коллекцией позитивных авто-тестов для REST API сервиса https://www.exchangerate-api.com"""
 
-    def test_get_exchange_rate_USD(self):
-        """"""
+    def test_exchange_rates_USD_positive(self):
+        """Тест проверки отправки GET-запроса для предоставления сведений о текущем курсе доллара США (USD) по отношению
+         к другим валютам. Валидация теста успешна в случае получения от сервера положительного кода состояния HTTP(200)
+         , ответ содержит JSON-объект с данными об актуальных обменных курсах валют по отношению к 1$(USD)."""
 
         status, result = ER.get_exchange_rate("USD")
 
-        assert status == 200
+        assert status == 200, f"\nКод состояния ответа{status}. Проверьте параметры запроса "
         assert result["base_code"] == "USD"
-        print(f"\n{result['conversion_rates']['RUB']}")
+        assert result["USD"] != result['conversion_rates']['RUB'], "Проверьте"
 
 
 

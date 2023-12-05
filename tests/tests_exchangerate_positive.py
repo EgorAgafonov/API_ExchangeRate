180b0073328a03332f87bf5c15ace93d5077495f
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-12-05 23:00:03 +0300

    File updated

--- tests/tests_exchangerate_positive.py
+++ tests/tests_exchangerate_positive.py
@@ -1,22 +1,29 @@
-import pytest
+import time
+
 from exchange_app import ExchangeRateAPI
+from time import asctime
+
 
 ER = ExchangeRateAPI()
 
 
 class TestExchangeRatePositive:
-    """Класс с коллекцией позитивных авто-тестов для REST API сервиса https://www.exchangerate-api.com"""
+    """Класс с коллекцией позитивных тестов для REST API сервиса https://www.exchangerate-api.com"""
 
     def test_exchange_rates_USD_positive(self):
         """Тест проверки отправки GET-запроса для предоставления сведений о текущем курсе доллара США (USD) по отношению
-         к другим валютам. Валидация теста успешна в случае получения от сервера положительного кода состояния HTTP(200)
-         , ответ содержит JSON-объект с данными об актуальных обменных курсах валют по отношению к 1$(USD)."""
+         к другим валютам. Валидация теста успешна в случае, если ответ сервера содержит положительный кода состояния
+        (200), ответ содержит JSON-объект с данными об актуальных обменных курсах валют по отношению к 1$(USD)."""
 
         status, result = ER.get_exchange_rate("USD")
 
-        assert status == 200, f"\nКод состояния ответа{status}. Проверьте параметры запроса "
+        assert status == 200, f"\nЗапрос отклонен, код состояния ответа{status}. Проверьте параметры запроса."
+        assert result["result"] == "success", (f"\nЗапрос отклонен, код состояния ответа{status}. Проверьте параметры "
+                                               f"запроса.")
         assert result["base_code"] == "USD"
-        assert result["USD"] != result['conversion_rates']['RUB'], "Проверьте"
+        assert result['conversion_rates']["USD"] < result['conversion_rates']['RUB'], "Ущипните себя и всё проверьте ;)"
+
+
 
 
 

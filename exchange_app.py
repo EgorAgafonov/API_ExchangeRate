95646987e837e9f01272316513153e46063b186e
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-12-05 16:54:39 +0300

    File updated

--- exchange_app.py
+++ exchange_app.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 import pytest
 from settings import api_key_valid
@@ -7,3 +9,17 @@ class ExchangeRateAPI:
     def __init__(self):
         self.base_url = "https://v6.exchangerate-api.com/v6/YOUR-API-KEY/" + api_key_valid
 
+    def get_exchange_rate(self, currency):
+        """Отправка стандартного GET-запроса для предоставления сведений о текущем курсе выбранной валюты по отношению к
+        валютам других стран."""
+
+        response = requests.get(self.base_url + "/latest" + currency)
+
+        status = response.status_code
+        result = ""
+        try:
+            result = response.json()
+        except json.JSONDecodeError:
+            result = response.text
+
+        return status, result
